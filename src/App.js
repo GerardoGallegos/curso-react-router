69a3ca2204249c9f270ec75ea66f6f5111b3ae5c
Gerardo Gallegos <puxerop@gmail.com>
2019-05-16 18:53:45 -0500

    leccion 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Hola = () => (
+  <h1>Hola</h1>
+)
+
+const Productos = () => (
+  <h1>Productos</h1>
+)
 
 const App = () => (
-  <div>
-    <h1>React Router *****</h1>
-  </div>
+  <BrowserRouter>
+     <>
+       <Route path='/hola' component={Hola} />
+       <Route path='/productos' component={Productos} />
+     </>
+  </BrowserRouter>
 )
 
 export default App
+
