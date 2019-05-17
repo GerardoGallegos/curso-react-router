f5f314ba2ccb75a1616bd93fb155af2e8b6d86a0
Gerardo Gallegos <puxerop@gmail.com>
2019-05-16 19:16:34 -0500

    Leccion 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const Hola = () => (
@@ -9,10 +9,20 @@ const Productos = () => (
   <h1>Productos</h1>
 )
 
+const Home = () => (
+  <h1>Productos</h1>
+)
+
+const HolaNinja = () => (
+  <h1>Hola Ninja</h1>
+)
+
 const App = () => (
   <BrowserRouter>
      <>
+       <Route path='/' exact component={Home} />
        <Route path='/hola' component={Hola} />
+       <Route path='/hola/ninja' exact component={HolaNinja} />
        <Route path='/productos' component={Productos} />
      </>
   </BrowserRouter>
