79db60d4afc0fe758ce6929e9014d7564cf0f53d
Gerardo Gallegos <puxerop@gmail.com>
2019-05-18 13:57:24 -0500

    Leccion 15

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Route, NavLink, Redirect } from 'react-router-dom'
+import './App.css'
 
-const Pagina = (props) => {
-  console.log(props)
+const Home = (props) => (
+  <h2>Home</h2>
+)
+
+const Login = ({ location }) => {
+  if (location.state) {
+    return <h2>{ location.state.message } </h2>
+  }
+
   return (
-    <h2>Pagina</h2>
+    <h2>Login</h2>
   )
 }
 
-class App extends Component {
-  state = {
-    user: {
-      name: 'Gerardo Ninja',
-      country: 'Mexico'
-    }
-  }
+const isAuth = true
 
-  render () {
-    return (
-      <BrowserRouter>
-        <>
-          <Route
-            path='/:page'
-            render={(props) => (
-              <Pagina
-                { ...props }
-                user={this.state.user}
-              />
-            )}
-          />
-        </>
-      </BrowserRouter>
-    )
-  }
+const Perfil = () => {
+  return isAuth
+    ? <h2>Bienvenido a tu Perfil</h2>
+    : <Redirect to={{
+      pathname: '/login',
+      state: {
+        message: 'Debes de hacer login para acceder a tu perfil'
+      }
+    }} />
 }
+
+const Navegation = () => (
+  <nav>
+    <NavLink to='/' exact activeClassName='activa'>Home</NavLink>
+    <NavLink to='/perfil' activeClassName='activa'>Perfil</NavLink>
+    <NavLink to='/login' activeClassName='activa'>Login</NavLink>
+  </nav>
+)
+
+const App = () => (
+  <BrowserRouter>
+    <>
+      <Navegation />
+      <Route path='/' exact render={Home} />
+      <Route path='/perfil' render={Perfil} />
+      <Route path='/login' render={Login} />
+    </>
+  </BrowserRouter>
+)
 
 export default App
