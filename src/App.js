db0cdf68595f20bbd8ec3102880970dd07f1eba9
Gerardo Gallegos <puxerop@gmail.com>
2019-05-18 09:11:04 -0500

    Leccion 11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React, { Fragment } from 'react'
-import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom'
-import queryString from 'query-string'
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css'
 
-const Productos = ({ location }) => {
-  // const query = new URLSearchParams(location.search)
-  // const color = query.get('color')
-  // const talla = query.get('talla')
+const Home = () => (
+  <h2>Pagina Principal</h2>
+)
 
-  const query = queryString.parse(location.search)
-  console.log(query)
-  return (
-    <h2>Color: { query.color } - Talla: { query.talla }</h2>
-  )
+class Usuarios extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => {
+        this.setState({ users })
+      })
+  }
+
+  render () {
+    const { users } = this.state
+    return (
+      <div>
+        <h2>Usuarios</h2>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              <Link to={{
+                pathname: `/usuarios/${user.id}`,
+                state: {
+                  user
+                }
+              }}>
+                { user.name }
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class UsuariosDetalle extends Component {
+  state = {
+    user: {}
+  }
+
+  componentDidMount () {
+    const { id } = this.props.match.params
+    
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(res => res.json())
+      .then(user => {
+        this.setState({ user })
+      })
+  }
+
+  render () {
+    const { user } = this.state
+    return (
+      <div>
+        <h2>Detalles de usuario</h2>
+        <h3>{ user.name }</h3>
+        <div>Email: { user.email }</div>
+        <div>Sitio Web: { user.website }</div>
+        <div>Telefono: { user.phone }</div>
+      </div>
+    )
+  }
 }
 
 const App = () => (
   <BrowserRouter>
     <>
-      <Route path='/productos' exact render={Productos} />
+      <Route path='/' exact render={Home} />
+      <Route path='/usuarios' exact component={Usuarios} />
+      <Route path='/usuarios/:id' component={UsuariosDetalle} />
     </>
   </BrowserRouter>
 )
